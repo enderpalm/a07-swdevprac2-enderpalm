df533a38f16931363445dc93eccc6a92b511c092
Palm •-• <ch.enderpalm@gmail.com>
2025-03-13 10:22:22 +0700

    feat: add `Select Venue` button navigating to /venue using router

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 export default function Banner() {
   const bannerList: string[] = ["cover", "cover2", "cover3", "cover4"];
   const [banner, setBanner] = useState<number>(0);
+  const router = useRouter();
 
   return (
     <div className="flex m-0 w-full h-[60vh] relative justify-center items-center">
@@ -29,6 +31,16 @@ export default function Banner() {
           tailored to your needs.
         </h2>
       </div>
+
+      <button
+        onClick={(e) => {
+          e.stopPropagation();
+          router.push("/venue");
+        }}
+        className="absolute bottom-4 right-4 py-2 px-4 rounded-lg text-white font-jetbrains bg-[rgba(255,255,255,0.1)] backdrop-blur-sm backdrop-saturate-100 border-solid border-[1px] border-[rgba(255,255,255,0.2)] hover:border-[3px] transition-all duration-75"
+      >
+        Select Venue
+      </button>
     </div>
   );
 }
